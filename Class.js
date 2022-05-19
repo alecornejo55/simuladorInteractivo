ab71df4b15acf388648f535c06a94ff5535a591f
Alex <alex.isaias.cornejo@gmail.com>
2022-05-19 17:13:51 -0300

    Agregando arrays

---
+++ Class.js
@@ -0,0 +1,8 @@
+class Registro {
+    constructor(precio, medioPago, total){
+        this.precio = precio;
+        this.medioPago = medioPago;
+        this.total = total;
+    }
+}
+export { Registro }
--- script.js
+++ script.js
@@ -1,25 +1,24 @@
+import { Registro } from "./Class.js";
+const historial = [];
+
 const calcularMonto = (precio, medioPago) => {
-    let total=0;
-    switch(medioPago){
-        case 1:
-            total = precio * 0.9;
-            break;
-        case 2:
-            total = precio * 1.1;
-            break;
-        case 3:
-            total = precio * 1.2;
-            break;
-        case 4:
-            total = precio * 1.3;
-            break;
+    let valorInteres = 1.3;
+    if(medioPago == 1){
+        valorInteres = 0.9;        
     }
-    return total;
+    if(medioPago == 2){
+        valorInteres = 1.1;        
+    }
+    if(medioPago == 3){
+        valorInteres = 1.2;        
+    }
+    return (precio * valorInteres).toFixed(2);
 }
 const init = () => {
     let precio, medioPago;
     let error = true
     while(error){
+        let total;
         precio = parseFloat(prompt("Ingrese un valor a calcular"));
         medioPago = parseInt(prompt(`Elija un medio de pago: \n1- Efectivo (10% de descuento)\n2- Tarjeta de crédito 1 cuota (10% de interés)\n3- Tarjeta de crédito 3 cuotas (20% de interés)\n4- Tarjeta de crédito 12 cuotas (30% de interés)`))
         if((isNaN(precio) || precio <= 0)){
@@ -30,7 +29,9 @@ const init = () => {
             alert("Medio de pago incorrecto");
             continue;
         }
-        alert(`Precio: $${precio}\nMedio de pago: ${medioPago}\nTotal: $${calcularMonto(precio, medioPago)}`);
+        total = calcularMonto(precio, medioPago);
+        historial.push(new Registro(precio, medioPago, total));
+        alert(`Precio: $${precio}\nMedio de pago: ${medioPago}\nTotal: $${total}`);
         error = false;
     }
 }
@@ -40,5 +41,13 @@ while(continuar){
     init();
     if(!confirm("¿Desea calcular otro monto?")){
         continuar = false;
+        if(historial.length===0){
+            break;
+        }
+        if(confirm("Proceso terminado. ¿Desea ver el historial de precios?")){
+            let textoMostrar = "Historial:\n";
+            historial.map((row)=>textoMostrar += `• Precio: ${row.precio} - Medio de pago: ${row.medioPago} - Total: ${row.total}\n`);
+            alert(textoMostrar);
+        }
     }
 }
